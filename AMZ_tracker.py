9eb33d9bf32f5564efec152e9da0265132491880
Gabriel Levine <gabelev@users.noreply.github.com>
2015-06-10 16:30:44 -0400

    Updated AMZ_tracker
    Outputs both pickle and pipe-delimited files. also links ups to a mongoDB.

--- AMZ_tracker.py
+++ AMZ_tracker.py
@@ -1,22 +1,23 @@
 from bs4 import BeautifulSoup
 import urllib2
 import csv
-# import pandas as pd
+import pandas as pd
 import time
 import lxml
-import sys
+import datetime
+import pickle
 import requests
 import re
-from retry import retry as _retry
+import sys
+import os
 import pymongo
 
-#insert curated endpoints here
-from endpoints import Best_Amish, Category_Amish, Search_Amish
+from retry import retry as _retry
+from endpoints import Search_Business, Search_Amish, Category_Amish, Best_Amish
+from asins import business_search_asins, business_tracking, amish_asin
 
 reload(sys)
 sys.setdefaultencoding("utf-8")
-
-prh_asin = ['xyz', 'abc']
 
 def iter_elements_extract_or_skip_attributes(elements, *attributes):
     for element in elements:
@@ -24,8 +25,8 @@ def iter_elements_extract_or_skip_attributes(elements, *attributes):
         if all(values):
             yield values
 
-def check_asin(result_list):
-    new_result = [item for item in result_list if item[1] in prh_asin]
+def check_asin(result_list, isbn_set):
+    new_result = [item for item in result_list if item[1] in isbn_set]
     return new_result
 
 def remove_prefix(s, prefix):
@@ -40,27 +41,23 @@ def convert_index(result_list):
 def urlopen_with_retry(url):
     return urllib2.urlopen(url)
 
-@_retry(requests.ConnectionError, tries=4, delay=3, backoff=2)
-def request_with_retry(url):
-    return requests.get(url)
-
-def Amz_Search_Results(endpoint):
+def Amz_Search_Results(endpoint, isbn_set):
     amz = urlopen_with_retry(endpoint)
     soup = BeautifulSoup(amz, "lxml")
     findone = BeautifulSoup(str(soup.find_all("li", class_="s-result-item")))
     elements = findone.find_all('li')
     iter_data = iter_elements_extract_or_skip_attributes(elements, 'id', 'data-asin')
-    return [item for item in iter_data if item[1] in prh_asin]
+    return [item for item in iter_data if item[1] in isbn_set]
 
-def Amz_Search_Results_alt(endpoint):
+def Amz_Search_Results_alt(endpoint, isbn_set):
     amz = urlopen_with_retry(endpoint)
     soup = BeautifulSoup(amz, "lxml")
     findone = BeautifulSoup(str(soup.find_all("div", attrs={"id": "mainResults"})))
     elements = findone.find_all('div')
     iter_data = iter_elements_extract_or_skip_attributes(elements, 'id', 'name')
-    return [item for item in iter_data if item[1] in prh_asin]
+    return [item for item in iter_data if item[1] in isbn_set]
 
-def Amz_bestseller(endpoint):
+def Amz_bestseller(endpoint, isbn_set):
     r = requests.get(endpoint)
     soup = BeautifulSoup(str(r.text), "html")
     findtwo = BeautifulSoup(str(soup.find_all("div", attrs={"class": "zg_itemImmersion"})))
@@ -74,59 +71,134 @@ def Amz_bestseller(endpoint):
     for b in findtwo.find_all("span", attrs={"class": "asinReviewsSummary acr-popover"}):
         x = b['name']
         asin_list.append(x)
-    final_result = check_asin(zip(rank_list, asin_list, title_list))
+    final_result = check_asin(zip(rank_list, asin_list, title_list), isbn_set)
     return final_result
 
-def run_spider_bestseller(item_search):
+def run_spider_run(item_search, isbn_set):
     results_dict = {}
-    #runs through the list of endpoints
     for key, value in item_search.iteritems():
         results = []
         for url in value:
-            try:
+            # try:
+            time.sleep(0.5)
+            x = Amz_Search_Results(url, isbn_set)
+            if x == []:
                 time.sleep(0.5)
-                x = Amz_bestseller(url)
-                results.extend(x)
-            except Exception as e:
-                 print e
+                x = Amz_Search_Results_alt(url, isbn_set)
+            results.extend(convert_index(x))
             results_dict[key] = results
     return results_dict
 
-def run_spider_run(item_search):
+def run_spider_bestseller(item_search, isbn_set):
     results_dict = {}
     for key, value in item_search.iteritems():
         results = []
         for url in value:
             time.sleep(0.5)
-            x = Amz_Search_Results(url)
-            if x == []:
-                time.sleep(0.5)
-                x = Amz_Search_Results_alt(url)
-            results.extend(convert_index(x))
+            x = Amz_bestseller(url, isbn_set)
+            results.extend(x)
             results_dict[key] = results
     return results_dict
 
-def format_data(data_name, output):
+def reformat_data_add_column(data_name, output_name, rank_type, tracking_asin, database_collection):
     df = pd.DataFrame.from_dict(data_name, orient='index')
-    new_df = df.transpose()
-    new_df.to_csv('Bestseller_Amish_debug.csv')
+    data = df.transpose()
+    results = []
+    today = datetime.datetime.now()
+    for index, row in data.iteritems():
+        for old_cell in row:
+            if pd.notnull(old_cell):
+                cell = str(old_cell)
+                cell = cell.split(',')
+                rank = cell[0][1:]
+                number = cell[1][2:-2]
+                tracking = 'no'
+                if number in tracking_asin:
+                    tracking = 'yes'
+                x = index, rank, number, rank_type, str(today), tracking
+                results.append(x)
+    name = output_name + "_" + str(today) + '.csv'
+    myfile = open(name, 'wb')
+    wr = csv.writer(myfile, delimiter='|', quoting= csv.QUOTE_NONE)
+    head = ('Terms', 'Rank', 'ASIN/ISBN', 'Rank Type', 'Date/time Collected', 'tracking')
+    wr.writerow(head)
+    for row in results:
+        wr.writerow(row)
+    print("----- MSG: CSV output")
+    results_d = {}
+    results_d[str(today)] = results
+    database_collection.insert(results_d)
+    print("----- MSG: DATABASE Write")
+    print("----- MSG: NEXT PROCESS STARTING")
+
+def reformat_data_add_column_bestseller(data_name, output_name, rank_type, tracking_asin, database_collection):
+    df = pd.DataFrame.from_dict(data_name, orient='index')
+    data = df.transpose()
+    results = []
+    today = datetime.datetime.now()
+    for index, row in data.iteritems():
+        for old_cell in row:
+            if pd.notnull(old_cell):
+                cell = str(old_cell)
+                cell = cell.split(',')
+                rank = cell[0][1:]
+                number = cell[1][2:-1]
+                tracking = 'no'
+                if number in tracking_asin:
+                    tracking = 'yes'
+                x = index, rank, number, rank_type, str(today), tracking
+                results.append(x)
+    name = output_name + "_" + str(today) + '.csv'
+    myfile = open(name, 'wb')
+    wr = csv.writer(myfile, delimiter='|', quoting= csv.QUOTE_NONE)
+    head = ('Terms', 'Rank', 'ASIN/ISBN', 'Rank Type', 'Date/time Collected', 'tracking')
+    wr.writerow(head)
+    for row in results:
+        wr.writerow(row)
+    print("----- MSG: CSV output")
+    results_d = {}
+    results_d[str(today)] = results
+    database_collection.insert(results_d)
+    print("----- MSG: DATABASE Write")
+    print("----- MSG: NEXT PROCESS STARTING")
+
+def make_pickle(data_name, output_name):
+    today = datetime.datetime.now()
+    name = output_name + str(today) + ".p"
+    pickle.dump( data_name, open( name, "wb" ) )
+    print("----- MSG: python output")
 
 if __name__ == "__main__":
 
-    conn_1 = pymongo.MongoClient()
-    db = conn_1.amzdb
-    collection_1 = db.amish_bestseller
-    result_bestseller = run_spider_bestseller(Best_Amish)
-    collection_1.insert(result_bestseller)
+    home_msg = "----- AMAZON TRACKING"
+    db_msg = "----- MSG: DATABASE ACTIVE - MongoDB"
+    last_msg = "----- AMAZON TRACKING Complete. Have a nice day."
 
-    conn_2 = pymongo.MongoClient()
-    db2 = conn_2.amzdb
-    collection_2 = db2.amish_category
-    result_category = run_spider_run(Category_Amish)
-    collection_2.insert(result_category)
+    print(home_msg)
 
-    conn_3 = pymongo.MongoClient()
-    db3 = conn_3.amzdb
-    collection_3 = db3.amish_search
-    result_search = run_spider_run(Search_Amish)
-    collection_3.insert(result_search)
+    conn = pymongo.MongoClient()
+    db = conn.amzdb
+    collection_amish_bestseller = db.amish_bestseller
+    collection_amish_category = db.amish_category
+    collection_amish_search = db.amish_search
+    collection_business_search = db.business_search
+
+    print(db_msg)
+
+    business_result = run_spider_run(Search_Business, business_tracking)
+    make_pickle(business_result, "business_search")
+    reformat_data_add_column(business_result, 'business_search', 'search', business_tracking, collection_business_search)
+
+    amish_category_result = run_spider_run(Category_Amish, amish_asin)
+    make_pickle(amish_category_result, "amish_category")
+    reformat_data_add_column(amish_category_result, 'amish_category', 'category', amish_asin, collection_amish_category)
+
+    amish_search_result = run_spider_run(Search_Amish, amish_asin)
+    make_pickle(amish_search_result, "amish_search")
+    reformat_data_add_column(amish_search_result, 'amish_search', 'search', amish_asin, collection_amish_search)
+
+    amish_bestseller_result = run_spider_bestseller(Best_Amish, amish_asin)
+    make_pickle(amish_bestseller_result, "amish_best")
+    reformat_data_add_column_bestseller(amish_bestseller_result, 'amish_bestseller', 'bestseller', amish_asin, collection_amish_bestseller)
+
+    print(last_msg)
