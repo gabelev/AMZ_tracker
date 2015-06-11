b9dbff52f042e321f149ef5896ccb1728791e5e8
Gabriel Levine <gabelev@users.noreply.github.com>
2015-06-11 17:20:13 -0400

    Update AMZ_tracker.py

--- AMZ_tracker.py
+++ AMZ_tracker.py
@@ -19,6 +19,13 @@ from asins import business_search_asins, business_tracking, amish_asin
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
+# def url_builder(page_range, terms):
+#     base_url = 'http://www.amazon.com/s/rh=%2Ck%3A'
+#     page = '&page='
+#     plus = '+'
+#     search_terms = plus.join(terms.split())
+#     return [base_url + search_terms + page + str(i) for i in range(1, page_range)]
+
 def iter_elements_extract_or_skip_attributes(elements, *attributes):
     for element in elements:
         values = tuple(element.get(att, None) for att in attributes)
@@ -104,7 +111,7 @@ def reformat_data_add_column(data_name, output_name, rank_type, tracking_asin, d
     df = pd.DataFrame.from_dict(data_name, orient='index')
     data = df.transpose()
     results = []
-    today = datetime.datetime.now()
+    today = str(datetime.datetime.now())
     for index, row in data.iteritems():
         for old_cell in row:
             if pd.notnull(old_cell):
@@ -115,9 +122,9 @@ def reformat_data_add_column(data_name, output_name, rank_type, tracking_asin, d
                 tracking = 'no'
                 if number in tracking_asin:
                     tracking = 'yes'
-                x = index, rank, number, rank_type, str(today), tracking
+                x = index, rank, number, rank_type, today, tracking
                 results.append(x)
-    name = output_name + "_" + str(today) + '.csv'
+    name = output_name + "_" + today + '.csv'
     myfile = open(name, 'wb')
     wr = csv.writer(myfile, delimiter='|', quoting= csv.QUOTE_NONE)
     head = ('Terms', 'Rank', 'ASIN/ISBN', 'Rank Type', 'Date/time Collected', 'tracking')
@@ -126,7 +133,7 @@ def reformat_data_add_column(data_name, output_name, rank_type, tracking_asin, d
         wr.writerow(row)
     print("----- MSG: CSV output")
     results_d = {}
-    results_d[str(today)] = results
+    results_d[today[0:10]] = results
     database_collection.insert(results_d)
     print("----- MSG: DATABASE Write")
     print("----- MSG: NEXT PROCESS STARTING")
@@ -135,7 +142,7 @@ def reformat_data_add_column_bestseller(data_name, output_name, rank_type, track
     df = pd.DataFrame.from_dict(data_name, orient='index')
     data = df.transpose()
     results = []
-    today = datetime.datetime.now()
+    today = str(datetime.datetime.now())
     for index, row in data.iteritems():
         for old_cell in row:
             if pd.notnull(old_cell):
@@ -146,9 +153,9 @@ def reformat_data_add_column_bestseller(data_name, output_name, rank_type, track
                 tracking = 'no'
                 if number in tracking_asin:
                     tracking = 'yes'
-                x = index, rank, number, rank_type, str(today), tracking
+                x = index, rank, number, rank_type, today, tracking
                 results.append(x)
-    name = output_name + "_" + str(today) + '.csv'
+    name = output_name + "_" + today + '.csv'
     myfile = open(name, 'wb')
     wr = csv.writer(myfile, delimiter='|', quoting= csv.QUOTE_NONE)
     head = ('Terms', 'Rank', 'ASIN/ISBN', 'Rank Type', 'Date/time Collected', 'tracking')
@@ -157,34 +164,43 @@ def reformat_data_add_column_bestseller(data_name, output_name, rank_type, track
         wr.writerow(row)
     print("----- MSG: CSV output")
     results_d = {}
-    results_d[str(today)] = results
+    results_d[today[0:10]] = results
     database_collection.insert(results_d)
     print("----- MSG: DATABASE Write")
     print("----- MSG: NEXT PROCESS STARTING")
 
 def make_pickle(data_name, output_name):
-    today = datetime.datetime.now()
-    name = output_name + str(today) + ".p"
+    today = str(datetime.datetime.now())[0:10]
+    name = output_name + today + ".p"
     pickle.dump( data_name, open( name, "wb" ) )
     print("----- MSG: python output")
 
-if __name__ == "__main__":
+def business_search_runner():
+    print("----MSG: Running Business Search")
+    business_result = run_spider_run(Search_Business, business_tracking)
+    make_pickle(business_result, "business_search")
+    reformat_data_add_column(business_result, 'business_search', 'search', business_tracking, collection_business_search)
 
-    home_msg = "----- AMAZON TRACKING"
-    db_msg = "----- MSG: DATABASE ACTIVE - MongoDB"
-    last_msg = "----- AMAZON TRACKING Complete. Have a nice day."
+def amish_search_runner():
+    print("----MSG: Running Amish Search")
+    amish_search_result = run_spider_run(Search_Amish, amish_asin)
+    make_pickle(amish_search_result, "amish_search")
+    reformat_data_add_column(amish_search_result, 'amish_search', 'search', amish_asin, collection_amish_search)
 
-    print(home_msg)
+def amish_category_runner():
+    print("----MSG: Running Amish Category")
+    amish_category_result = run_spider_run(Category_Amish, amish_asin)
+    make_pickle(amish_category_result, "amish_category")
+    reformat_data_add_column(amish_category_result, 'amish_category', 'category', amish_asin, collection_amish_category)    
 
-    conn = pymongo.MongoClient()
-    db = conn.amzdb
-    collection_amish_bestseller = db.amish_bestseller
-    collection_amish_category = db.amish_category
-    collection_amish_search = db.amish_search
-    collection_business_search = db.business_search
+def amish_bestseller_runner():
+    print("----MSG: Running Amish Bestseller")
+    amish_bestseller_result = run_spider_bestseller(Best_Amish, amish_asin)
+    make_pickle(amish_bestseller_result, "amish_best")
+    reformat_data_add_column_bestseller(amish_bestseller_result, 'amish_bestseller', 'bestseller', amish_asin, collection_amish_bestseller)
 
-    print(db_msg)
-
+def all_runner():
+    print("----MSG: Running All")
     business_result = run_spider_run(Search_Business, business_tracking)
     make_pickle(business_result, "business_search")
     reformat_data_add_column(business_result, 'business_search', 'search', business_tracking, collection_business_search)
@@ -200,5 +216,43 @@ if __name__ == "__main__":
     amish_bestseller_result = run_spider_bestseller(Best_Amish, amish_asin)
     make_pickle(amish_bestseller_result, "amish_best")
     reformat_data_add_column_bestseller(amish_bestseller_result, 'amish_bestseller', 'bestseller', amish_asin, collection_amish_bestseller)
+
+
+if __name__ == "__main__":
+
+    home_msg = "----- MISMI: AMAZON TRACKING TOOL \n ---- by G.Levine"
+    db_msg = "----- MSG: DATABASE ACTIVE - MongoDB"
+    last_msg = "----- AMAZON TRACKING Complete. Have a nice day."
+
+    print(home_msg)
+
+    conn = pymongo.MongoClient()
+    db = conn.amzdb
+    collection_amish_bestseller = db.amish_bestseller
+    collection_amish_category = db.amish_category
+    collection_amish_search = db.amish_search
+    collection_business_search = db.business_search
+
+    print(db_msg)
+    print("Which tracker would you like to run?")
+    print("All, Business Search, All Amish, Amish Search, Amish Category, Amish Bestseller?")
+    action = raw_input("> ")
+    if action == "All":
+        all_runner()
+    if action == "Business Search":
+        business_search_runner()
+    if action == "Amish Search":
+        amish_search_runner()
+    if action == "Amish Category":
+        amish_category_runner()
+    if action == "Amish Bestseller":
+        amish_bestseller_runner()
+    if action == "All Amish":
+        print("Running through all Amish")
+        amish_category_runner()
+        amish_search_runner()
+        amish_bestseller_runner()
+    else:
+        print("please input again")
 
     print(last_msg)
